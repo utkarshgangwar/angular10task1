6cccd9bced111a8ddc5b58b2ae10947d374b5550
Utkarsh Gangwar <utkarshgangwar22@gmail.com>
2023-01-10 12:56:17 +0530

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms'
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  firstname: string = '';
+  name: string = '';
+  email: string = '';
+  password: string = '';
+  mobile: string = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  title = 'Sign Up';
+  disclaimer = 'Rougly Created Form';
+
+  addFormInfo(formValue: NgForm){
+    console.log(formValue.value);
+  }
+
+
+}
